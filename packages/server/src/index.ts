09a417bb782b96877c6d7ae722ca3701dbe46675
Lucas Coelho <lucascoelhodacosta@gmail.com>
2021-05-08 18:38:35 -0300

    feat(server): socket.io middleware for username

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,18 +3,33 @@ import {
   server,
   gameLoop,
 } from './server';
-import { Dictionary } from './types';
+import { User } from './server/classes';
+import { Dictionary, UserSocket } from './types';
 
-const users: Dictionary<Socket> = {};
+const users: Dictionary<User> = {};
 
 function connectUser(socket: Socket) {
   // TODO: Create a separate class structure to handling explicitly with connected users.
-  users[socket.id] = socket;
+  users[socket.id] = new User((<UserSocket>socket).username, socket.id);
 }
 
 function broadcastServerSnapshot() {
   // console.log('update!');
 }
+
+server.use((socket, next) => {
+  const { nickname } = socket.handshake.auth;
+  if (!nickname) {
+    return next(new Error('invalid nickname!'));
+  }
+  /**
+     * We have to ignore this so we can implement this middlware as suggested in socket.io
+     * documentation
+     */
+  // eslint-disable-next-line no-param-reassign
+  (<UserSocket>socket).username = nickname;
+  next();
+});
 
 server.on('connection', (socket: Socket) => {
   connectUser(socket);
--- packages/server/src/types/index.ts
+++ packages/server/src/types/index.ts
@@ -1,7 +1,14 @@
+import { Socket } from 'socket.io';
+
 interface Dictionary<T> {
   [Key: string]: T;
 }
 
+interface UserSocket extends Socket {
+  username: string;
+}
+
 export {
   Dictionary,
+  UserSocket,
 };
